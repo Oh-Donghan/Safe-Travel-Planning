ca96f50bd835aef2c945b0f49a012a3999215b33
Oh-Donghan <odonghan94@gmail.com>
2024-05-07 19:21:28 +0900

    start project

---
+++ annyeong/src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TravelAlarmLogger from './components/Sample'
+
+function App() {
+
+  return (
+    <>
+      <div>Safe Travel Planning</div>
+      <TravelAlarmLogger />
+    </>
+  )
+}
+
+export default App
